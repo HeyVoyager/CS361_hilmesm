91cfe2b68f72cb1c7f22f07483706c4845329539
Michael Hilmes <hilmes.m@gmail.com>
2021-11-01 17:35:51 -0600

    Third_commit

--- planet_gazing.py
+++ planet_gazing.py
@@ -1,44 +1,23 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import SearchForm
 import json
+from table_scraper import scraper
 
-# Code modified from Corey Shafer's YouTube Series
+# Code modified from Corey Schafer's YouTube Series
 # Python Flask Tutorial: Full-Featured Web App
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '8475930875648334'
 
-with open('planet_visibility.json', 'r') as myfile:
-    data = myfile.read()
-
-print(type(data))
-dump = json.dumps(data)
-print(type(dump))
-print(data)
-
-# posts = [
-#     {
-#         'planet': 'Mercury',
-#         'rise': '6:00 pm',
-#         'comment': 'Barely visible'
-#     },
-#     {
-#         'planet': 'Venus',
-#         'rise': '7:00 pm',
-#         'comment': 'Clearly visible'
-#     }
-# ]
+# with open('planet_visibility.json', 'r') as myfile:
+#     data = myfile.read()
 #
-# posts_2 = [
-#     {'planet': 'Mercury', 'rise': 'Sat 6:01 am',  'comment': 'Slightly difficult to see'},
-#     {'planet': 'Venus', 'rise': 'Fri 11:58 am', 'comment': 'Great visibility'},
-#     {'planet': 'Mars', 'rise': 'Sat 7:12 am', 'comment': 'Extremely difficult to see'},
-#     {'planet': 'Jupiter', 'rise': 'Fri 3:45 pm', 'comment': 'Perfect visibility'},
-#     {'planet': 'Saturn', 'rise': 'Fri 3:02 pm', 'comment': 'Perfect visibility'},
-#     {'planet': 'Uranus', 'rise': 'Fri 6:43 pm', 'comment': 'Average visibility'},
-#     {'planet': 'Neptune', 'rise': 'Fri 4:50 pm', 'comment': 'Difficult to see'}
-# ]
+# print(type(data))
+# dump = json.dumps(data)
+# print(type(dump))
+# print(data)
+
 
 weather = [
     {'low': '55', 'high': '75',  'median': '65'}
@@ -51,12 +30,30 @@ def hello():
 
 @app.route("/visibility")
 def visibility():
+    with open('planet_visibility.json', 'r') as myfile:
+        data = myfile.read()
+
+    print(type(data))
+    dump = json.dumps(data)
+    print(type(dump))
+    print(data)
     return render_template('visibility.html', weather=weather, jsonfile=json.loads(data), title='Tonight')
 
 @app.route("/search", methods=['GET', 'POST'])
 def search():
     form = SearchForm()
     if form.validate_on_submit():
+
+        if '-' in form.location.data:
+            input = form.location.data
+            input = input.replace(' ', '')
+            url = 'https://www.timeanddate.com/astronomy/night/@' + input
+            scraper(url)
+        else:
+            input = form.location.data
+            url = 'https://www.timeanddate.com/astronomy/night/@z-us-' + input
+            scraper(url)
+
         flash(f'Search successful for {form.location.data}!', 'success')
         return redirect(url_for('visibility'))
     return render_template('search.html', title='Search', form=form)
