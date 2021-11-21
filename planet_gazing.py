8d3449236ce9bc7ea640b935395a4fc59e9e0142
Michael Hilmes <hilmes.m@gmail.com>
2021-11-21 12:02:07 -0600

    Eighth_commit - Code cleanup

--- planet_gazing.py
+++ planet_gazing.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, flash, redirect, jsonify, request
+from flask_cors import CORS
 from forms import SearchForm
 import json
 from table_scraper import scraper
@@ -8,34 +9,33 @@ import requests
 # Python Flask Tutorial: Full-Featured Web App
 
 app = Flask(__name__)
-
+CORS(app)
 app.config['SECRET_KEY'] = '8475930875648334'
-
-weather = [
-    {'low': '55', 'high': '75',  'median': '65'}
-]
 
 @app.route("/")
 @app.route("/home")
 def hello():
+    """Route handler for home page"""
     return render_template('home.html')
 
 @app.route("/visibility")
 def visibility():
+    """Route handler for planet visibility page"""
+    # Read the planet visibility data from json file
     with open('planet_visibility.json', 'r') as myfile:
         data = myfile.read()
-
+    # Read the weather data from json file
     with open('weather.json', 'r') as weatherfile:
         weather_conditions = weatherfile.read()
-    print(weather_conditions)
     return render_template('visibility.html', weather_conditions=json.loads(weather_conditions), jsonfile=json.loads(data), title='Tonight')
 
 @app.route("/search", methods=['GET', 'POST'])
 def search():
+    """Route handler for search page"""
     form = SearchForm()
     if form.validate_on_submit():
         # Generates send_data in JSON format
-        if '-' in form.location.data:
+        if '-' in form.location.data:     # If lat/long data...
             input = form.location.data
             input = input.replace(' ', '')
             input = input.split(',')
@@ -46,7 +46,7 @@ def search():
             latlon = {'latitude': input[0], 'longitude': input[1]}
             res = requests.post(url, json=latlon)
             res = res.json()
-        else:
+        else:                             # Else if zip code data...
             zipcode = form.location.data
             send_data = {}
             send_data['zipCode'] = zipcode
@@ -54,68 +54,67 @@ def search():
             zipdata = {'zipCode': zipcode}
             res = requests.post(url, json=zipdata)
             res = res.json()
-
+        # Reset the weather.json file
         f = open("weather.json", "r+")
         f.seek(0)
         f.truncate()
+        # Write the weather.json file with new weather data
         with open('weather.json', 'w') as fout:
             json.dump(res, fout)
-
+        # Call scraper function with the location to scrape data for
         scraper(form.location.data)
-        # if '-' in form.location.data:
-        #     input = form.location.data
-        #     input = input.replace(' ', '')
-        #     url = 'https://www.timeanddate.com/astronomy/night/@' + input
-        #     scraper(url)
-        # else:
-        #     input = form.location.data
-        #     url = 'https://www.timeanddate.com/astronomy/night/@z-us-' + input
-        #     scraper(url)
-
+        # Flash success message
         flash(f'Search successful for {form.location.data}!', 'success')
+        # Redirect to visibility page
         return redirect(url_for('visibility'))
+    # Re-render the search page if unsuccessful
     return render_template('search.html', title='Search', form=form)
+
+@app.route("/top5", methods=["GET"])
+def top5_stargazing_coordinates():
+    """
+    Route handler for delivering top 5 planet gazing sites
+    (Source: https://thedyrt.com/magazine/local/dark-sky-map-stargazing-oregon/)
+    1. Green Lakes lat 44.0873 lon 121.7310
+    2. Pine Mountain Observatory lat 43.7917 lon 120.9410
+    3. Crater Lake lat 42.9446 lon 122.1090
+    4. Zumwalt Prairie lat 45.5559 lon 116.9587
+    5. Cannon Beach lat 45.8918 lon 123.9615
+    """
+    return jsonify([
+        {"latitude": 44.0873, "longitude": 121.7310},
+        {"latitude": 43.7917, "longitude": 120.9410},
+        {"latitude": 42.9446, "longitude": 122.1090},
+        {"latitude": 45.5559, "longitude": 116.9587},
+        {"latitude": 45.8918, "longitude": 123.9615}
+    ])
 
 @app.route("/planet-gazing-data/latitude-longitude", methods=['GET', 'POST'])
 def planet_gazing_latlong():
+    """Route handler for delivering planet gazing data by lat/long"""
     req_data = request.get_json()
     latitude = req_data['latitude']
     longitude = req_data['longitude']
 
-    # latitude = request.args.get('latitude')
-    # longitude = request.args.get('longitude')
     string = str(latitude) + ',' + str(longitude)
     scraper(string)
 
     with open('planet_visibility.json', 'r') as myfile:
         data = myfile.read()
-
-    # print(type(data))
-    dump = json.dumps(data)
-    # print(type(dump))
-    # print(data)
-    # return data
     return '''<body>{}</body>'''.format(data)
-#
-#
+
+
 @app.route("/planet-gazing-data/zip-code", methods=['GET', 'POST'])
 def planet_gazing_zipcode():
+    """Route handler for delivering planet gazing data by zip code"""
     req_data = request.get_json()
     zipCode = req_data['zipCode']
 
-
-    # zipCode = request.args.get('zipCode')
     string = str(zipCode)
     scraper(string)
 
     with open('planet_visibility.json', 'r') as myfile:
         data = myfile.read()
-
-    print(type(data))
-    dump = json.dumps(data)
-    print(type(dump))
-    print(data)
-    # return data
     return '''<body>{}</body>'''.format(data)
 
 @app.route("/json_example", methods=['POST'])
@@ -129,30 +128,37 @@ def write_weather(weather):
 
 @app.route("/mercury")
 def mercury():
+    """Route handler for Mercury information page"""
     return render_template('mercury.html')
 
 @app.route("/venus")
 def venus():
+    """Route handler for Venus information page"""
     return render_template('venus.html')
 
 @app.route("/mars")
 def mars():
+    """Route handlder for Mars information page"""
     return render_template('mars.html')
 
 @app.route("/jupiter")
 def jupiter():
+    """Route handler for Jupiter information page"""
     return render_template('jupiter.html')
 
 @app.route("/saturn")
 def saturn():
+    """Route handler for Saturn information page"""
     return render_template('saturn.html')
 
 @app.route("/uranus")
 def uranus():
+    """Route handler for Uranus information page"""
     return render_template('uranus.html')
 
 @app.route("/neptune")
 def neptune():
+    """Route handler for Neptune information page"""
     return render_template('neptune.html')
 
 if __name__ == '__main__':
--- table_scraper.py
+++ table_scraper.py
@@ -9,31 +9,33 @@ import os
 # https://medium.com/analytics-vidhya/how-to-web-scrape-tables-online-using-python-and-beautifulsoup-36d5bafeb982
 # How to Web Scrape Tables Online, Using Python and BeautifulSoup by Christopher Zita
 
-
-
 def scraper(input_data):
-
+    """Table scraper that scrapes planet visibility data from timeanddate.com
+    and generates a .json file with data for planet rise, set, meridian, and additional info"""
+    # If '-' in string, must be lat/long data
     if '-' in input_data:
         input = input_data
         input = input.replace(' ', '')
         url = 'https://www.timeanddate.com/astronomy/night/@' + input
-
+    # Otherwise, must be a zip code
     else:
         input = input_data
         url = 'https://www.timeanddate.com/astronomy/night/@z-us-' + input
 
+    # Reset the .json file
     f = open("planet_visibility.json", "r+")
     f.seek(0)
     f.truncate()
 
+    # Get the url for the corresponding planet visibility data
     requests.get(url)
     page = requests.get(url)
-
     soup = BeautifulSoup(page.text, 'lxml')
-    # print(soup)
 
+    # Find the table with the matching class
     table_data = soup.find('table', class_='tb-wc zebra sep fw')
 
+    # Find all of the table headers
     headers = []
     count = 0
     for i in table_data.find_all('th'):
@@ -49,6 +51,7 @@ def scraper(input_data):
 
     df = pd.DataFrame(columns=headers)
 
+    # Find all of the table rows
     for j in table_data.find_all('tr')[2:]:
             row_data = j.find_all('td')
             row = [tr.text.strip() for tr in row_data]
@@ -57,26 +60,15 @@ def scraper(input_data):
 
     json_list = df.to_json(orient='records')
 
-    # print(df)
-    # print(json_list)
-
     json_list = json.loads(json_list)
 
+    # Add planet name to the list of .json objects
     planets = ['Mercury', 'Venus', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
     count = 0
     for item in json_list:
         item['Planet'] = planets[count]
         count += 1
 
-    # print(json_list)
-
+    # Write planet_visibility.json file with scraped data
     with open('planet_visibility.json', 'w') as fout:
         json.dump(json_list, fout)
-
-    with open('planet_visibility.json', 'r') as myfile:
-        data = myfile.read()
-
-    # print(data)
-
-# url = 'https://www.timeanddate.com/astronomy/night/usa/portland-or'
-# scraper(url)
